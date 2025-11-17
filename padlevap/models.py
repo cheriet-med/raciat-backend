90424c1e3fb4b93e13395e06c0b02a498fc86df8
cheriet-med <cheriet-mohamed@hotmail.com>
2025-11-17 23:23:42 +0100

    updates

--- padlevap/models.py
+++ padlevap/models.py
@@ -387,6 +387,7 @@ class Order(models.Model):
     region = models.CharField(max_length=500,blank=True, null=True)
     location = models.CharField(max_length=500,blank=True, null=True)
     is_read = models.BooleanField(default=False)
+    phone = models.CharField(max_length=500,blank=True, null=True)
 
     def __str__(self):
         return f"Order #{self.id}"
